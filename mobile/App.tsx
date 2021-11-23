b53920c1c16da5e15daea5f3af7cfe8ce7b9907c
antoniotex <antoniotx.dev@gmail.com>
2021-11-23 00:32:14 -0300

    Inicia projeto mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>App works!!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
